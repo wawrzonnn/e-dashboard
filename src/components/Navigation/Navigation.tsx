709ad1b0828cb7cb6c505f00a8e49f7f1d2bb8bc
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-05-23 20:56:18 +0200

    test

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import styles from './Navigation.module.scss';
 import { Container } from 'components/Container/Container';
-import { Link, Tabs, Tab, TabContent } from 'nerdux-ui-system';
+import { Link, Tabs, Tab } from 'nerdux-ui-system';
+
 export const Navigation = () => {
    return (
       <Container>
-         <div className={styles.wrapper}>
-            <div className={styles.tabs}>
-               {/* <Tabs defaultOpenedId='1'>
-                  <Tab id={'1'}>1</Tab>
-                  <Tab id={'2'}>2</Tab>
-            </Tabs> */}
+         <div className={styles.navigation__wrapper}>
+            <div className={styles.navlink__wrapper}>
+               <Tabs>
+                  <Tab id={'1'}>Dashboard</Tab>
+                  <Tab id={'2'}>Leads</Tab>
+               </Tabs>
             </div>
-            <div className={styles.login}>
+            <div className={styles.login__wrapper}>
                <p>
                   Logged in as: <span>karolina@nerdbord.io</span>
                </p>
