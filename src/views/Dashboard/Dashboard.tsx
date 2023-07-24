ecd8c0e2a1e3fb558b124b8edfaab03423d12479
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-07-24 17:34:48 +0200

    feat: add leads chart

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import styles from './Dashboard.module.scss';
 import { LeadsList } from 'components/LeadsList/LeadsList';
-import { LeadsChart } from 'components/LeadsList/LeadsChart';
+import { LeadsChart } from 'components/LeadsChart/LeadsChart';
+
 export const Dashboard = () => {
    return (
       <div className={styles.wrapper}>
          <h2>Dashboard</h2>
-         <LeadsList />
-         <LeadsChart />
+         <div className={styles.leads__wrapper}>
+            <div className={styles.leadslist__wrapper}>
+               <LeadsList />
+            </div>
+            <LeadsChart />
+         </div>
       </div>
    );
 };
